51fc64c9f42b8b1d379a904d3e7e7f36897e5c8d
reidpat <61036489+reidpat@users.noreply.github.com>
2022-02-07 18:20:57 -0800

    New habit creation

--- src/components/Habits.svelte
+++ src/components/Habits.svelte
@@ -2,12 +2,15 @@
 import { onMount } from "svelte";
 import { user } from "../sessionStore";
 import {supabase} from "../supabaseClient"
-import { Button } from "carbon-components-svelte";
+import { Button, Modal } from "carbon-components-svelte";
 import Add16 from "carbon-icons-svelte/lib/Add16";
 
 let habits = [];
+let open = false;
 
 let loading = false;
+
+let newHabitName = "";
 
 onMount(async ()=>{
     let {data:Habits, error, status} = await supabase
@@ -62,8 +65,10 @@ async function newHabit(){
     const { data, error } = await supabase
   .from('Habits')
   .insert([
-    { name: 'test_value', user_id: $user.id},
+    { name: newHabitName, user_id: $user.id},
   ])
+
+  newHabitName=""
 }
 </script>
 <div class="content-container">
@@ -83,17 +88,33 @@ async function newHabit(){
         {/if}
     </div>
     {/each}
-    <Button on:click={newHabit} icon={Add16}></Button>
+    <Button on:click={()=>open=true} icon={Add16}></Button>
+    <Modal
+  bind:open
+  size="xs"
+  modalHeading="New Habit Name"
+  primaryButtonText="Confirm"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (open = false)}
+  on:open
+  on:close
+  on:click:button--primary={()=>{open = false; newHabit()}}
+>
+  <input bind:value={newHabitName}/>
+</Modal>
 </div>
 
 <style>
     .card{
         margin: 50px;
         padding: 10px;
-        box-shadow: 1px 1px 1px 1px grey;
+        box-shadow: 1px 1px 1px 1px rgba(255, 255, 255, 0.452);
+        background-color: rgba(128, 128, 128, 0.411);
+        border-radius: 2px;
     }
     .clickable{
         cursor: pointer;
+        user-select:none;
     }
 </style>
 

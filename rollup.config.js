a97e479e950d01e5ac9e57d3f7df5222482c9bd9
reidpat <61036489+reidpat@users.noreply.github.com>
2022-04-15 11:13:42 -0700

    env vars trouble

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,17 @@ import { config } from 'dotenv';
 import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json'
 import preprocess from 'svelte-preprocess';
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
     plugins: [
         replace({
             __api: JSON.stringify({
                 env: {
-                    isProd: production,
                     ...config().parsed // attached the .env config
                 }
             }),
+            isProduction: production,
             delimiters: ['', '']
         }),
         json(),
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			define: {
+			  'process.env': process.env,
+			},
+		  },
 	},
 	preprocess: preprocess()
 };

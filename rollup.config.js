caaaf25aacf6d8bef76f088966001589bfdf16c8
reidpat <61036489+reidpat@users.noreply.github.com>
2022-04-15 11:07:03 -0700

    added scss

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
 import { config } from 'dotenv';
 import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json'
+import preprocess from 'svelte-preprocess';
 
 export default {
-  plugins: [
-  replace({
-          __api: JSON.stringify({
-              env: {
-                  isProd: production,
-                  ...config().parsed // attached the .env config
-              }
-          }),
-          delimiters: ['', '']
-      }),
-      json(),
-  // ...
-  ],
-  // ...
+    plugins: [
+        replace({
+            __api: JSON.stringify({
+                env: {
+                    isProd: production,
+                    ...config().parsed // attached the .env config
+                }
+            }),
+            delimiters: ['', '']
+        }),
+        json(),
+        svelte({
+            /* ... */
+            preprocess: preprocess()
+        }),
+        // ...
+        // ...
+    ],
+
 }

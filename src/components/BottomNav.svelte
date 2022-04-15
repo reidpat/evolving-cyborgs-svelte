f9804bb886efbaae7cfaccd84f85e781f87c1670
reidpat <61036489+reidpat@users.noreply.github.com>
2022-04-15 11:07:17 -0700

    bottom menu basic stlying

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -5,7 +5,22 @@ function handleClick(name){
 }
 </script>
 
-<div>
-<a href="/" class="clickable" on:click={() => handleClick('')}> <h3>Home</h3></a>
+<div id="bottom-nav-wrapper">
+<a href="/" class="clickable" on:click={() => handleClick('')}> <h3>Habits</h3></a>
 <a href="/vices" class="clickable" on:click={() => handleClick('vices')}> <h3>Vices</h3> </a> 
 </div>
+
+<style lang="scss">
+    #bottom-nav-wrapper{
+        position: fixed;
+        bottom: 0px;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        background-color: #1d1c1f;
+
+        a {
+            margin: 20px;
+        }
+    } 
+</style>

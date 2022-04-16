8a08784169d8723508f9904dce0824dce2ee8032
reidpat <61036489+reidpat@users.noreply.github.com>
2022-04-15 20:14:20 -0700

    redirect fixes

--- src/components/SignIn.svelte
+++ src/components/SignIn.svelte
@@ -2,9 +2,13 @@
     import { supabase } from '../supabaseClient'
 
 async function signIn() {
+  let redirect = 'evolvingcyborgs.netlify.app';
+  if(!process.env.ROLLUP_WATCH){
+    redirect = 'http://localhost:3000'
+  }
   const { user, session, error } = await supabase.auth.signIn(
     { provider: 'google' },
-    { redirectTo: 'http://localhost:3000' }
+    { redirectTo: redirect}
   )
   
 }

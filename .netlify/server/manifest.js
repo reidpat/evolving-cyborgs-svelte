b7c21cb82e17109c6003487dcfda961f69b9c796
reidpat <61036489+reidpat@users.noreply.github.com>
2022-04-24 20:09:23 -0700

    quick more changes to manifest

--- .netlify/server/manifest.js
+++ .netlify/server/manifest.js
@@ -28,7 +28,7 @@ const manifest = {
   assets: /* @__PURE__ */ new Set(["cyborg.png", "favicon.png", "manifest.json", "meditation_192.png", "meditation_512.png", "service-worker.js"]),
   _: {
     mime: { ".png": "image/png", ".json": "application/json" },
-    entry: { "file": "start-2f30944f.js", "js": ["start-2f30944f.js", "chunks/index-d41602b9.js", "chunks/index-ffc5a9d1.js"], "css": [] },
+    entry: { "file": "start-6ec77214.js", "js": ["start-6ec77214.js", "chunks/index-d41602b9.js", "chunks/index-ffc5a9d1.js"], "css": [] },
     nodes: [
       () => Promise.resolve().then(() => __toESM(require("./nodes/0.js"))),
       () => Promise.resolve().then(() => __toESM(require("./nodes/1.js"))),

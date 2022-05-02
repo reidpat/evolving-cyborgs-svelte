676c42c10ef66962a8a9b5d6afb4f4dacd053e5a
reidpat <61036489+reidpat@users.noreply.github.com>
2022-05-01 21:40:09 -0700

    habits changed and working with updates

--- src/components/Habits.svelte
+++ src/components/Habits.svelte
@@ -32,8 +32,10 @@
 			.from(`habits:user_id=eq.${userID}`)
 			.on('*', (payload) => {
 				console.log('change', payload);
-				console.log(loading);
-				updateHabitUI(updateTimeline(payload.new));
+				let newHabit = matchHabit(payload.new);
+				console.log(newHabit);
+				//setTimeout(updateHabitUI(newHabit), 1000);
+				//updateHabitsData();
 			})
 			.subscribe();
 		
@@ -50,6 +52,45 @@
 
 		await resetHabits();
 	});
+
+	
+	//Given a habit, find the matching one in the store already and mesh the new one with the old timeline.
+	function matchHabit(newHabit){
+		let mergedHabit = null;
+		habits.forEach(habit => {
+			if(habit.id == newHabit.id){
+				mergedHabit = {...newHabit, timeline: habit.timeline}
+			}
+		})
+
+		return mergedHabit;
+	}
+
+	function matchHabitList(habits, newHabit, newTimeline){
+		let found = false;
+		habits = habits.map((habit) => {
+				if (newHabit.id !== habit.id) {
+					return { ...habit };
+				} else {
+					found = true;
+					if(newTimeline){
+						return updateTimeline({...newHabit, timeline: newTimeline});
+					}
+					return updateTimeline({...newHabit, timeline: habit.timeline});
+				}
+			});
+			if (!found) {
+				habits = [...habits, { ...newHabit }];
+			}
+		return habits;
+	}
+
+	function updateHabitUI(newHabit, timeline) {
+			console.log('timeline', timeline);
+			habits = matchHabitList(habits, newHabit, timeline);
+			console.log('ui update', habits);
+			habitStore.set(habits);
+	}
 
 	//taking in current progress and goal and returning what the new goal should be
 	//3, 5, 10, 15, 20, 30, ...
@@ -93,7 +134,8 @@
 				eventDate.setHours(0,0,0,0)
 				return eventDate > threshold;
 			})
-			return {...habit, goalProgress: filteredTimeline.length}
+			console.log('filtered', filteredTimeline);
+			return {...habit, goalProgress: filteredTimeline.length, goal: getNewHabitGoal(filteredTimeline.length, habit.goal)}
 
 	}
 
@@ -155,22 +197,7 @@
 		}
 	}
 
-	function updateHabitUI(newHabit) {
-			let found = false;
-			habits = habits.map((habit) => {
-				if (newHabit.id !== habit.id) {
-					return { ...habit };
-				} else {
-					found = true;
-					return { ...newHabit, timeline: habit.timeline };
-				}
-			});
-			console.log(habits);
-			habitStore.set(habits);
-			if (!found) {
-				habits = [...habits, { ...newHabit }];
-			}
-	}
+	
 	async function completeHabit(habit) {
 		if (!loading) {
 			let streak;
@@ -186,8 +213,10 @@
 			let newGoal = getNewHabitGoal(goalProgress, habit.goal);
 			let newHabit = { ...habit, is_complete: !habit.is_complete, streak, goalProgress, goal: newGoal};
 
-			updateHabitUI(newHabit);
+			
 			loading = true;
+
+			updateHabitUI(newHabit);
 
 			if (newHabit.is_complete) {
 				//add to timeline
@@ -195,31 +224,54 @@
 				let xp = (newHabit.streak + habit.goalProgress) * 10
 				dispatch('addXp', {xp: xp, event: habit.name});
 
+				// updateHabitUI(newHabit, [{
+				// 	created_at: new Date(),
+				// 	xp
+				// }, ...newHabit.timeline]);
+
 				const { data: timeline, error } = await supabase
 					.from('timeline')
 					.insert([{ user_id: $user.id, habit: habit.id, xp_awarded: xp}]);
 
+				
 				const { data: habit_update } = await supabase
 					.from('habits')
 					.update({ is_complete: newHabit.is_complete, streak, goal: newHabit.goal})
 					.eq('id', habit.id);
+
+				updateHabitUI(habit_update[0], [...newHabit.timeline, timeline[0]]);
+				
+
+				
 			} else {
 				let xp = newHabit.timeline[newHabit.timeline.length - 1].xp_awarded;
-			
+				dispatch('addXp', {xp: -xp, event: habit.name});
 
 				if (habit.timeline && habit.timeline.length > 0) {
+					console.log('deleting timeline:', newHabit.timeline[newHabit.timeline.length - 1]);
 					const { data: timeline_update, error } = await supabase
 						.from('timeline')
 						.delete()
 						.eq('id', newHabit.timeline[newHabit.timeline.length - 1].id);
+
+					if(error){
+					console.log('delete-error', error);
 				}
+
+				}
+
+
+
+				let timeline = [...newHabit.timeline].slice(0, newHabit.timeline.length - 1);
+				updateHabitUI(newHabit, timeline);
 
 				const { data: habit_update } = await supabase
 					.from('habits')
 					.update({ is_complete: newHabit.is_complete, streak,  goal: newHabit.goal})
 					.eq('id', habit.id);
+				
 
-				dispatch('addXp', {xp: -xp, event: habit.name});
+				
 			}
 
 			loading = false;
